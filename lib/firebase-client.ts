46adfbedc888402b4886c2d4611f8be99d489b8e
Ikageng Masie <ikymasie@gmail.com>
2026-02-18 18:56:10 +0200

    ready

--- lib/firebase-client.ts
+++ lib/firebase-client.ts
@@ -2,15 +2,25 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// }; 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-}; 
+  apiKey: "AIzaSyDEbq-e54hhgHrhIB6qVX8t8QlCysGPAIw",
+  authDomain: "kika-bw.firebaseapp.com",
+  projectId: "kika-bw",
+  storageBucket: "kika-bw.firebasestorage.app",
+  messagingSenderId: "1054148523063",
+  appId: "1:1054148523063:web:5f51c5a21268f47db7fbb2",
+  measurementId: "G-03TBRC18WQ"
+};
+
 // Initialize Firebase client SDK
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
